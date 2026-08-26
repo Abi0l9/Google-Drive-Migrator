5eaba38dc3d1d09b989f01a77a65d27656053257
Monsur Oyedeji <102850720+Abi0l9@users.noreply.github.com>
2026-08-26 15:57:04 +0100

    Configure Next.js for OpenNext free runtime

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     serverActions: { bodySizeLimit: "2mb" },
   },
 };
+
+initOpenNextCloudflareForDev();
 
 export default nextConfig;
